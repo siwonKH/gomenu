be8d84187b098eaf32607f5c3e1cacd14395c959
siwonKH <kanghyunkim313@gmail.com>
2022-03-25 02:16:48 +0900

    code location change

--- request/menu.go
+++ request/menu.go
@@ -5,17 +5,25 @@ import (
 	"fmt"
 	"github.com/siwonKH/gomenu/handler"
 	"github.com/siwonKH/gomenu/model"
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func SearchMenu(schoolCode string, aptCode string, date string, date2 string, KEY string) (model.NeisMenu, error) {
+	var menuData, blank model.NeisMenu
 	urlIn := "https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json"
 	urlIn += "&KEY=" + KEY + "&ATPT_OFCDC_SC_CODE=" + aptCode + "&SD_SCHUL_CODE=" + schoolCode + "&MLSV_FROM_YMD=" + date + "&MLSV_TO_YMD=" + date2
 
-	var menuData, blank model.NeisMenu
 	resp, err := http.Get(urlIn)
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}(resp.Body)
 	if err != nil {
 		return blank, handler.HandleErr("(menu)request failed: " + aptCode + schoolCode)
 	}
@@ -27,12 +35,12 @@ func SearchMenu(schoolCode string, aptCode string, date string, date2 string, KE
 	if err != nil {
 		return blank, handler.HandleErr("(menu)json unmarshal failed: " + aptCode + schoolCode)
 	}
+
 	if menuData.MealServiceDietInfo == nil {
 		return blank, handler.HandleErr("(menu)menu not found: " + aptCode + schoolCode)
 	}
 	if menuData.MealServiceDietInfo[0].Head[0].ListTotalCount < 1 {
 		return blank, fmt.Errorf("no school menu")
 	}
-
 	return menuData, nil
 }
--- request/school.go
+++ request/school.go
@@ -4,19 +4,27 @@ import (
 	"encoding/json"
 	"github.com/siwonKH/gomenu/handler"
 	"github.com/siwonKH/gomenu/model"
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 )
 
 func SearchSchool(schoolName string, aptCode string, maxRes string, KEY string) (model.NeisSchool, error) {
+	var schoolData, blank model.NeisSchool
 	schoolNameUrl := url.QueryEscape(schoolName)
 	urlIn := "https://open.neis.go.kr/hub/schoolInfo?Type=json&pSize=" + maxRes
 	urlIn += "&SCHUL_NM=" + schoolNameUrl + "&ATPT_OFCDC_SC_CODE=" + aptCode + "&KEY=" + KEY
 
-	var schoolData, blank model.NeisSchool
 	resp, err := http.Get(urlIn)
-	defer resp.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}(resp.Body)
 	if err != nil {
 		return blank, handler.HandleErr("(school)request failed: " + schoolName)
 	}
@@ -28,9 +36,9 @@ func SearchSchool(schoolName string, aptCode string, maxRes string, KEY string) 
 	if err != nil {
 		return blank, handler.HandleErr("(school)json unmarshal failed: " + schoolName)
 	}
+
 	if schoolData.SchoolInfo[0].Head[0].ListTotalCount < 1 {
 		return blank, handler.HandleErr("(school)no school found: " + schoolName)
 	}
-
 	return schoolData, nil
 }
