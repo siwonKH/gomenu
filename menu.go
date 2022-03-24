ec708e13613f6150e900a85d4fcba8d782f43cba
siwonKH <kanghyunkim313@gmail.com>
2022-03-25 02:12:42 +0900

    added new func

--- menu.go
+++ menu.go
@@ -28,14 +28,25 @@ func (m *GoMenu) MenuByRange(schoolCode string, aptCode string, date string, dat
 	return searchMenu(schoolCode, aptCode, date, date2, m)
 }
 
+func (m *GoMenu) YesterdayMenu(schoolCode string, aptCode string) (model.Menu, error) {
+	tomorrow := time.Now().AddDate(0, 0, -1).Format("20060102")
+	return searchMenu(schoolCode, aptCode, tomorrow, tomorrow, m)
+}
+
 func (m *GoMenu) TodayMenu(schoolCode string, aptCode string) (model.Menu, error) {
 	today := time.Now().Format("20060102")
 	return searchMenu(schoolCode, aptCode, today, today, m)
 }
 
-func (m *GoMenu) NextDayMenu(schoolCode string, aptCode string) (model.Menu, error) {
+func (m *GoMenu) TomorrowMenu(schoolCode string, aptCode string) (model.Menu, error) {
 	tomorrow := time.Now().AddDate(0, 0, 1).Format("20060102")
 	return searchMenu(schoolCode, aptCode, tomorrow, tomorrow, m)
+}
+
+func (m *GoMenu) MonthMenu(schoolCode string, aptCode string, month int, year int) (model.Menu, error) {
+	startMonth := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.Local)
+	endMonth := startMonth.AddDate(0, 1, 0).AddDate(0, 0, -1)
+	return searchMenu(schoolCode, aptCode, startMonth.Format("20060102"), endMonth.Format("20060102"), m)
 }
 
 func (m *GoMenu) ThisMonthMenu(schoolCode string, aptCode string) (model.Menu, error) {
